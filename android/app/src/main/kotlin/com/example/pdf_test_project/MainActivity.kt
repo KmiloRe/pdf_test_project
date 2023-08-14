58e4fd3feb1a4c49f14cb70661a111732462cc50
josereyes1 <josereyes@tyba.com.co>
2023-08-14 07:03:20 -0500

    initial commit

---
+++ android/app/src/main/kotlin/com/example/pdf_test_project/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.pdf_test_project
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/home_page.dart
@@ -0,0 +1,72 @@
+import 'dart:io';
+import 'dart:typed_data';
+import 'package:path_provider/path_provider.dart';
+import 'package:pdf/pdf.dart';
+import 'package:pdf/widgets.dart' as pw;
+import 'package:flutter/material.dart';
+import 'package:printing/printing.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final pdf = pw.Document();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  void savePdf() async {
+    final doc = await _generatePdf(PdfPageFormat.a4, 'Test');
+    final output = await getTemporaryDirectory();
+    final file = File('${output.path}/example.pdf');
+    await file.writeAsBytes(doc);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: PdfPreview(
+        build: (format) => _generatePdf(format, 'Test'),
+      ),
+    );
+  }
+
+  Future<Uint8List> _generatePdf(PdfPageFormat format, String title) async {
+    final pdf = pw.Document(version: PdfVersion.pdf_1_5, compress: true);
+    final font = await PdfGoogleFonts.nunitoExtraLight();
+
+    pdf.addPage(
+      pw.Page(
+        pageFormat: format,
+        build: (context) {
+          return pw.Column(
+            children: [
+              pw.SizedBox(
+                width: double.infinity,
+                child: pw.FittedBox(
+                  child: pw.Text(title, style: pw.TextStyle(font: font)),
+                ),
+              ),
+              pw.SizedBox(height: 20),
+              pw.Flexible(child: pw.FlutterLogo())
+            ],
+          );
+        },
+      ),
+    );
+
+    return pdf.save();
+  }
+}
